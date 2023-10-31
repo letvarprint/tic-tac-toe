2a20499aab60acabfce2bef09c0faf8d010fab25
letvarprint <a.brixencov@icloud.com>
2023-11-01 01:51:49 +0200

    Initial Commit

---
+++ tic tac toe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tic tac toe
+//
+//  Created by Alexandru Brixencov on 01.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
