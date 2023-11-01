6d7c4c32f52b77cde061893c77413b9100a22013
Denis Stashkov <themoldex@gmail.com>
2023-11-01 22:27:47 +0300

    Initial Commit

---
+++ 123123/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  123123
+//
+//  Created by Денис Сташков on 1.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
