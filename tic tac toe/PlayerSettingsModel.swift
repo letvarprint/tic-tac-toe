5f305232edfd51f64793facee244c13e21f15523
Denis Stashkov <themoldex@gmail.com>
2023-11-03 19:57:55 +0300

    добавил 2 модели

---
+++ tic tac toe/PlayerSettingsModel.swift
@@ -0,0 +1,13 @@
+//
+//  WelcomeScreenModel.swift
+//  tic tac toe
+//
+//  Created by Денис Сташков on 3.11.2023.
+//
+
+import Foundation
+
+struct ChoiсePlayer {
+    let playerOne: String
+    let playerTwo: String
+}
---
+++ tic tac toe/TableViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TableViewModel.swift
+//  tic tac toe
+//
+//  Created by Денис Сташков on 3.11.2023.
+//
+
+import Foundation
